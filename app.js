d6962a1b0dd7521cc5c6066ec5d873ab306bc49c
Chris Bascom <bascomchris@gmail.com>
2023-01-16 13:32:10 -0800

    location start templet

---
+++ app.js
@@ -0,0 +1,32 @@
+let seattle = {
+    max: , 65
+    min: , 23
+    sale: , 6.3
+
+}
+
+let tokyo = {
+    max: , 24 
+    min: , 3
+    sale: , 1.2
+
+}
+
+let dubai = {
+    max: ,   38
+    min: ,   11
+    sale: ,  3.7
+}
+
+let paris = {
+    max: ,   38
+    min: ,   20
+    sales: , 2.3
+}
+
+let Lima = {
+    max: ,  16
+    min: ,  2   
+    sale: , 4.6
+
+}
